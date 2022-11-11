f93ee334f56e8b6b5d509d0a4063e9ae7b66445b
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-11-11 02:27:22 -0600

    add IngredienService interface with implementations and tests

---
+++ src/main/java/com/ernesto/springrecipeapp/services/IngredientService.java
@@ -0,0 +1,8 @@
+package com.ernesto.springrecipeapp.services;
+
+import com.ernesto.springrecipeapp.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
